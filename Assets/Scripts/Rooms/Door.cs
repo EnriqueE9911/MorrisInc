e0c4286aebfd8a19f54fe84b7129b98c837f71e1
Enrique Espinosa <enrique.espinosa99@gmail.com>
2022-10-25 19:39:10 -0500

    Completo hasta video 9

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private Transform previousRoom;
-    [SerializeField] private Transform nextsRoom;
+    [SerializeField] private Transform nextRoom;
     [SerializeField] private CameraController cam;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -12,13 +12,17 @@ public class Door : MonoBehaviour
         {
             if(collision.transform.position.x < transform.position.x) 
             {
-                Debug.Log("cam " + cam, cam.gameObject);
-                Debug.Log("room " + nextsRoom, nextsRoom.gameObject);
-                cam.MoveToNewRoom(nextsRoom);
+                cam.MoveToNewRoom(nextRoom);
+                nextRoom.GetComponent<Room>().ActivateRoom(true);
+                previousRoom.GetComponent<Room>().ActivateRoom(false);
+            }
+            else
+            {
+                cam.MoveToNewRoom(previousRoom);
+                previousRoom.GetComponent<Room>().ActivateRoom(true);
+                nextRoom.GetComponent<Room>().ActivateRoom(false);
             }
                 
-            else
-                cam.MoveToNewRoom(previousRoom);
         }
     }
 
